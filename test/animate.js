4edbf470dc4650dd40ad32a65d1940b9426dc7ed
Guillaume <gui.lebas@gmail.com>
2019-12-08 19:50:07 +0100

    fix(test/animate) Add unit tests for error cases

--- test/animate.js
+++ test/animate.js
@@ -1,6 +1,7 @@
 
 import animate from '../src/animate'
 import assert from 'assert'
+import { errors } from '../src/error'
 import { performance } from 'perf_hooks'
 
 const createElement = (element = { style: {} }) => ({
@@ -36,12 +37,35 @@ let element
 beforeEach(() => element = createElement())
 
 describe('animate()', () => {
+    it('should throw when updating Animation.currentTime from not null to null', () => {
+        assert.throws(() => animate(element, keyframes, 1).currentTime = null, errors.CURRENT_TIME_UNRESOLVED)
+    })
+    it('should throw when executing Animation.finish() while endTime === Infinity', () => {
+
+        const animation = animate(element, keyframes, { duration: 1, iterations: Infinity })
+
+        assert.throws(() => animation.finish(), errors.INVALID_STATE_FINISH)
+        animation.cancel()
+    })
+    it('should throw when executing Animation.pause() while currenTime === null && playbackRate < 0 && endTime === Infinity', () => {
+
+        const animation = animate(element, keyframes, { duration: 1, iterations: Infinity })
+
+        animation.cancel()
+        animation.playbackRate = -1
+        assert.throws(() => animation.pause(), errors.INVALID_STATE_PAUSE)
+    })
+    it('should throw when executing Animation.play() while endTime === Infinity && playbackRate < 0', () => {
+        assert.throws(
+            () => animate(element, keyframes, { direction: 'reverse', duration: 1, iterations: Infinity }),
+            errors.INVALID_STATE_PLAY)
+    })
     it('should return a new Animation w/ expected prop values before/after Animation.cancel()', () => {
 
         const animation = animate(element, keyframes, 1)
         const expected = {
             currentTime: 0,
-            id: 'Animation#1',
+            id: 'Animation#5',
             pending: true,
             playState: 'running',
             playbackRate: 1,
