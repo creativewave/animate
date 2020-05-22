90c06ca1105614263cae64253c7e917c56283778
Guillaume <gui.lebas@gmail.com>
2020-05-22 15:31:02 +0200

    fix(rollup.config) Switch to Rollup

---
+++ rollup.config.js
@@ -0,0 +1,54 @@
+
+import babel from 'rollup-plugin-babel'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import pkg from './package.json'
+import { terser } from 'rollup-plugin-terser'
+
+const { dependencies = {}, peerDependencies = {} } = pkg
+const external = id => (new RegExp(`^(${Object.keys({ ...dependencies, ...peerDependencies }).join('|')})`)).test(id)
+
+const getBabelConfig = targets => ({
+    exclude: /node_modules/,
+    presets: [['@babel/preset-env', {
+        corejs: 3,
+        // debug: true,
+        targets,
+        useBuiltIns: 'usage',
+    }]]
+})
+
+export default [
+    {
+        external,
+        input: 'src/index.js',
+        output: {
+            file: pkg.main,
+            format: 'cjs',
+        },
+        plugins: [babel(getBabelConfig({ node: true }))],
+    },
+    {
+        external,
+        input: 'src/index.js',
+        output: {
+            file: pkg.module,
+            format: 'es',
+        },
+        plugins: [babel(getBabelConfig({ esmodules: true }))],
+    },
+    {
+        input: 'src/index.js',
+        output: {
+            file: pkg.unpkg,
+            format: 'iife',
+            name: 'animate',
+        },
+        plugins: [
+            nodeResolve(),
+            commonjs(),
+            terser(),
+            babel(getBabelConfig('defaults')),
+        ],
+    },
+]
