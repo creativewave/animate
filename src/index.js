96deba43c7fc48e1208342f05a28183ddfc376bf
Guillaume <gui.lebas@gmail.com>
2020-09-25 06:18:08 +0200

    fix(src/index) Export Animation and KeyframeEffect

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 
 export { default } from './animate'
+export { default as Animation } from './animation'
 export { setAttribute, setProperty, setStyle } from './buffer'
+export { default as KeyframeEffect } from './effect'
 export { interpolateNumber, interpolateTaggedNumbers, tag } from './interpolate'
