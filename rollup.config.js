08f356bcb4a5079953d9775d20fd4bfb0cf26ac5
Guillaume <gui.lebas@gmail.com>
2020-10-25 15:11:24 +0100

    fix(package) Upgrade dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 
-import babel from 'rollup-plugin-babel'
+import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
 import nodeResolve from '@rollup/plugin-node-resolve'
 import pkg from './package.json'
