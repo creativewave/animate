fb0cfef1588e28d0abb906a1081e61b7f71b66e0
Guillaume <gui.lebas@gmail.com>
2020-11-04 10:44:33 +0100

    fix(__tests__/task) Add case cancel task while update another

--- __tests__/task.js
+++ __tests__/task.js
@@ -1,7 +1,7 @@
 
-import { animationFrame } from '../src/task'
+import { animationFrame, animationFrameGroup } from '../src/task'
 
-describe('raf', () => {
+describe('animationFrame(task)', () => {
     it('should stub window.requestAnimationFrame() and window.cancelAnimationFrame()', () => {
 
         const fn1 = jest.fn()
@@ -16,5 +16,29 @@ describe('raf', () => {
             expect(fn1).toHaveBeenCalledTimes(1)
             expect(fn2).not.toHaveBeenCalled()
         })
+    })
+})
+
+describe('animationFrameGroup(task)', () => {
+    it('should cancel a request while satisfying other requests', async () => {
+
+        const updateAnimation1 = jest.fn()
+        const updateAnimation2 = jest.fn()
+
+        // Start both animations
+        animationFrameGroup.request(updateAnimation1)
+        animationFrameGroup.request(updateAnimation2)
+
+        await Promise.resolve()
+
+        // Update animation 1
+        animationFrameGroup.request(updateAnimation1)
+        // Finish animation 2
+        animationFrameGroup.cancel(updateAnimation2)
+
+        await Promise.resolve()
+
+        expect(updateAnimation1).toHaveBeenCalledTimes(2)
+        expect(updateAnimation2).toHaveBeenCalledTimes(1)
     })
 })
