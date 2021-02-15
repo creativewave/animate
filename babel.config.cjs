0c01bf32d5a15e46d8a4ad1cf9983cf4c4c9b947
Guillaume <gui.lebas@gmail.com>
2021-02-15 16:51:14 +0100

    fix(babel.config) Optimize babel config

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,31 +1,33 @@
 
 const { dependencies } = require('./package.json')
+
 const plugins = [
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-private-methods',
     '@babel/plugin-proposal-private-property-in-object',
 ]
-const presetEnv = { corejs: dependencies['core-js'], useBuiltIns: 'usage' }
+const presetEnv = {
+    bugfixes: true,
+    corejs: { proposals: true, version: dependencies['core-js'] },
+    useBuiltIns: 'usage',
+}
 const presets = [['@babel/preset-env', presetEnv]]
 
 module.exports = api => {
 
     const env = api.env()
 
-    if (env === 'node') {
-        presetEnv.modules = false
-    }
-    if (env === 'browser' || env === 'development') {
-        presetEnv.targets = { esmodules: true }
-    } else {
-        presetEnv.targets = { node: true }
-    }
-
     if (env === 'browser') {
+        presetEnv.targets = { esmodules: true }
         return { exclude: /core-js/, plugins, presets }
     }
 
-    plugins.push(['@babel/plugin-transform-runtime', { version: dependencies['@babel/runtime'] }])
+    presetEnv.targets = { node: true }
+
+    if (env === 'node') {
+        plugins.push(['@babel/plugin-transform-runtime', { version: dependencies['@babel/runtime'] }])
+        presetEnv.modules = false
+    }
 
     return { exclude: /node_modules/, plugins, presets }
 }
