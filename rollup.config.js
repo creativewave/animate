4a4ea1d88d800d534017869b46d6787b1c3620ec
Guillaume <gui.lebas@gmail.com>
2021-03-07 09:10:09 +0100

    fix(package) Add Node CJS build

--- rollup.config.js
+++ rollup.config.js
@@ -4,20 +4,33 @@ import commonjs from '@rollup/plugin-commonjs'
 import nodeResolve from '@rollup/plugin-node-resolve'
 import replace from '@rollup/plugin-replace'
 import { terser } from 'rollup-plugin-terser'
-import { unpkg } from './package.json'
+import { dependencies, exports, unpkg } from './package.json'
 
-export default {
-    input: 'src/index.js',
-    output: {
-        file: unpkg,
-        format: 'umd',
-        name: 'animate',
-    },
-    plugins: [
-        replace({ 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV) }),
-        nodeResolve(),
-        babel({ babelHelpers: 'bundled' }),
-        commonjs(),
-        terser({ keep_fnames: /play|pause/ }),
-    ],
-}
+export default process.env.NODE_ENV === 'cjs'
+    ? {
+        input: 'src/index.js',
+        external: new RegExp(`^(${Object.keys(dependencies).join('|')})`),
+        output: {
+            file: exports['.'].require,
+            format: 'cjs',
+        },
+        plugins: [
+            babel({ babelHelpers: 'runtime' }),
+            commonjs(),
+        ],
+    }
+    : {
+        input: 'src/index.js',
+        output: {
+            file: unpkg,
+            format: 'umd',
+            name: 'animate',
+        },
+        plugins: [
+            replace({ 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV) }),
+            nodeResolve(),
+            babel({ babelHelpers: 'bundled' }),
+            commonjs(),
+            terser({ keep_fnames: /play|pause/ }),
+        ],
+    }
